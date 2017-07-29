992eed338818d7de8b2af4d52dcfd39c115879bb
Joralmo <JoralmoPro@Gmail.com>
2017-07-29 16:57:13 -0500

    ola k ace

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function(){
-	let form=$(".nombre");
 	let input=$("#nombre");
 	let CVS = document.createElement('canvas'), ctx = CVS.getContext('2d');
 	$("a#submit").on("click", function(){
@@ -7,7 +6,7 @@ $(function(){
 		var letras=nombre.split("");
 		var palabras=[];
 		CVS.width  = 500;
-		CVS.height = 700;
+		CVS.height = 300;
 		document.body.appendChild(CVS);
 		
 		ColocarPalabra();
@@ -16,7 +15,8 @@ $(function(){
 			var img = new Image();
 			img.onload = function(){
 				ctx.drawImage(img, 0, 0);
-				x=50;
+				x=(500-50*nombre.length)/2;
+				console.log(x);
 				i=1;
 				genero=$('input:checkbox[name=genero]:checked').val();
 				if(!genero){
@@ -24,24 +24,22 @@ $(function(){
 				}else{
 					genero="m";
 				}
-				console.log(genero);
 				for(letra of letras){
 					$.post('seleccionar.php', {letra:letra, genero:genero}, function(msg){
 						palabras=json=$.parseJSON(msg);
 						random=Math.floor((Math.random() * palabras.length-1) + 1);
-						y=50;
+						y=70;
 						i=0;
 						for(letra of palabras[random]){
 							if(i==0){
-								ctx.font = "50px Arial";
+								ctx.font = "50px Anton";
 								ctx.fillStyle = "rgba(250, 250, 250, 0.7)"
-								letra=letra.toUpperCase();
 							}else{
-								ctx.font = "30px Arial";
+								ctx.font = "30px Anton";
 								ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
 							}
 							ctx.fillText(letra, x, y);
-							y+=25;
+							y+=30;
 							i++;
 						}
 						x+=50;
@@ -49,6 +47,12 @@ $(function(){
 				}
 			};
 			img.src = "nombre.png";
+			$("h3").text("Resultado");
+			$(".container").fadeOut();
+			$("canvas").css({
+				marginLeft: '33%',
+				marginTop: '8%'
+			});
 		}
 	});
 })
